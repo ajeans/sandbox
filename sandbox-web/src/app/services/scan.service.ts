1d28752dfdd440eb74c7652a2e5d87d952fa1825
Arnaud Jeansen <arnaud.jeansen@gmail.com>
2017-03-06 16:49:50 +0100

    Use pipes and nice dates

--- sandbox-web/src/app/services/scan.service.ts
+++ sandbox-web/src/app/services/scan.service.ts
@@ -3,9 +3,9 @@ import {PRODUCTS} from "./product.service";
 import {Scan} from "../model/scan";
 
 const SCANS: Scan[] = [
-  { product: PRODUCTS[0], date: new Date()  },
-  { product: PRODUCTS[0], date: new Date()  },
-  { product: PRODUCTS[1], date: new Date()  }
+  { product: PRODUCTS[0], date: new Date('02/24/2017 19:25:00')  },
+  { product: PRODUCTS[0], date: new Date('02/21/2017 11:29:00')  },
+  { product: PRODUCTS[1], date: new Date('01/31/2017 23:15:00')  }
 ];
 
 @Injectable()
@@ -13,7 +13,7 @@ export class ScanService {
 
   getScans(): Promise<Scan[]> {
     return new Promise(resolve => {
-      setTimeout(() => resolve(SCANS), 3000);
+      setTimeout(() => resolve(SCANS), 2000);
     });
   }
 
