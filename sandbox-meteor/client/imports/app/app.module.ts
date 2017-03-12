37faa188358a534656038f3b7fca44e13f04a977
Arnaud Jeansen <arnaud.jeansen@gmail.com>
2017-03-12 18:50:04 +0100

    Now adding ionic to the mix (mess?).

--- sandbox-meteor/client/imports/app/app.module.ts
+++ sandbox-meteor/client/imports/app/app.module.ts
@@ -1,15 +1,21 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {MyApp} from './app.component';
+import {IonicModule, IonicApp, IonicErrorHandler} from 'ionic-angular';
 
 @NgModule({
     declarations: [
         MyApp
     ],
+    imports: [
+        IonicModule.forRoot(MyApp),
+    ],
+    bootstrap: [IonicApp],
     entryComponents: [
         MyApp
     ],
     providers: [
-        { provide: ErrorHandler }
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
     ]
 })
-export class AppModule {}
+export class AppModule {
+}
