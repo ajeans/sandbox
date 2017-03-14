9eaeda249799fca0e323b9b53deca6b3409e909c
Arnaud Jeansen <arnaud.jeansen@gmail.com>
2017-03-14 10:31:50 +0100

    Adding the chats page with the moment dep and do some styling.

--- sandbox-meteor/client/imports/app/app.module.ts
+++ sandbox-meteor/client/imports/app/app.module.ts
@@ -1,17 +1,20 @@
 import {NgModule, ErrorHandler} from '@angular/core';
 import {MyApp} from './app.component';
 import {IonicModule, IonicApp, IonicErrorHandler} from 'ionic-angular';
+import {ChatsPage} from "../pages/chats/chats";
 
 @NgModule({
     declarations: [
-        MyApp
+        MyApp,
+        ChatsPage
     ],
     imports: [
         IonicModule.forRoot(MyApp),
     ],
     bootstrap: [IonicApp],
     entryComponents: [
-        MyApp
+        MyApp,
+        ChatsPage
     ],
     providers: [
         {provide: ErrorHandler, useClass: IonicErrorHandler}
